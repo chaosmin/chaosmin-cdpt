00e06640f206ece943299fe554b5b8740abee8a0
Romani <chaosmin@hotmail.com>
2020-08-19 13:05:17 +0800

    🚚 move RequestUtil from web.utils to base.utils

--- src/main/kotlin/tech/chaosmin/framework/utils/RequestUtil.kt
+++ src/main/kotlin/tech/chaosmin/framework/utils/RequestUtil.kt
@@ -1,4 +1,4 @@
-package tech.chaosmin.framework.web.utils
+package tech.chaosmin.framework.utils
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page

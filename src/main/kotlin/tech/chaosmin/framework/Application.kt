792f514d786aa536c6adfd2de8bd6e1e22a9aab5
Romani <chaosmin@hotmail.com>
2020-08-19 17:50:20 +0800

    🔧 enable swagger2

--- src/main/kotlin/tech/chaosmin/framework/Application.kt
+++ src/main/kotlin/tech/chaosmin/framework/Application.kt
@@ -4,7 +4,9 @@ import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.transaction.annotation.EnableTransactionManagement
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+@EnableSwagger2
 @EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan("tech.chaosmin.framework.dao")
 @SpringBootApplication

a79f70901ff22cf09a9ee69114dc8ce3b77b7716
Romani <chaosmin@hotmail.com>
2020-12-01 17:22:14 +0800

    Merge code from origin/master

--- src/main/kotlin/tech/chaosmin/framework/config/WebMvcConfig.kt
+++ src/main/kotlin/tech/chaosmin/framework/config/WebMvcConfig.kt
@@ -1,6 +1,7 @@
 package tech.chaosmin.framework.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import tech.chaosmin.framework.web.interceptor.AuthInterceptor

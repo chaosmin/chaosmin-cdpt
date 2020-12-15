0f7f763210624c223958c4b5d4996c0f3b81cc32
Romani <chaosmin@hotmail.com>
2020-12-15 13:33:06 +0800

    格式化接口返回时间, 统一 'yyyy-MM-dd HH:mm:ss'

--- src/main/kotlin/tech/chaosmin/framework/domain/response/share/base/BaseShareResponseDTO.kt
+++ src/main/kotlin/tech/chaosmin/framework/domain/response/share/base/BaseShareResponseDTO.kt
@@ -1,16 +1,19 @@
 package tech.chaosmin.framework.domain.response.share.base
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import io.swagger.annotations.ApiModelProperty
 import java.io.Serializable
 import java.util.*
 
 open class BaseShareResponseDTO : Serializable {
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "创建时间")
     var createTime: Date? = null
 
     @ApiModelProperty(value = "创建人")
     var creator: String? = null
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @ApiModelProperty(value = "更新时间")
     var updateTime: Date? = null
 

9b3e10f6265e53e3eb3fa60c163c8360ed5e67d8
Romani <chaosmin@hotmail.com>
2020-12-23 21:10:45 +0800

    角色查询返回已分配的权限列表

--- src/main/kotlin/tech/chaosmin/framework/handler/logic/RoleQueryLogic.kt
+++ src/main/kotlin/tech/chaosmin/framework/handler/logic/RoleQueryLogic.kt
@@ -7,6 +7,7 @@ import tech.chaosmin.framework.dao.dataobject.Role
 import tech.chaosmin.framework.domain.PageQuery
 import tech.chaosmin.framework.domain.entity.RoleEntity
 import tech.chaosmin.framework.handler.logic.base.BaseQueryLogic
+import tech.chaosmin.framework.service.AuthorityService
 import tech.chaosmin.framework.service.RoleService
 
 /**
@@ -14,12 +15,18 @@ import tech.chaosmin.framework.service.RoleService
  * @since 2020/12/17 15:28
  */
 @Component
-class RoleQueryLogic(private val roleService: RoleService) : BaseQueryLogic<RoleEntity, Role> {
+class RoleQueryLogic(
+    private val roleService: RoleService,
+    private val authorityService: AuthorityService
+) : BaseQueryLogic<RoleEntity, Role> {
 
     override fun get(id: Long): RoleEntity? {
         val role = roleService.getById(id)
         return if (role == null) null
-        else RoleMapper.INSTANCE.convert2Entity(role)
+        else RoleMapper.INSTANCE.convert2Entity(role).apply {
+            val authorities = authorityService.findAuthorities(setOf(id))
+            this.authorityIds = authorities.mapNotNull { it.id }.toSet()
+        }
     }
 
     override fun page(cond: PageQuery<Role>): IPage<RoleEntity> {

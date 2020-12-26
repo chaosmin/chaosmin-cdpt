7241ee2786850a8c0aa2bd0f03618b4874b03588
Romani <chaosmin@hotmail.com>
2020-12-26 09:40:16 +0800

    允许服务注册发现

--- src/main/kotlin/tech/chaosmin/framework/Application.kt
+++ src/main/kotlin/tech/chaosmin/framework/Application.kt
@@ -3,11 +3,13 @@ package tech.chaosmin.framework
 import org.mybatis.spring.annotation.MapperScan
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Import
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import springfox.documentation.oas.annotations.EnableOpenApi
 
 @EnableOpenApi
+@EnableDiscoveryClient
 @EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan("tech.chaosmin.framework.dao")
 @Import(cn.hutool.extra.spring.SpringUtil::class)

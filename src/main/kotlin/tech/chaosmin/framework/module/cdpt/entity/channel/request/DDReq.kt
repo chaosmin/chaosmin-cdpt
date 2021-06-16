ff1da807ea610b935139520fd80cd38f167feed9
Romani <chaosmin@hotmail.com>
2021-06-16 11:28:00 +0800

    [对接]添加大地保险请求响应体

---
+++ src/main/kotlin/tech/chaosmin/framework/module/cdpt/entity/channel/request/DDReq.kt
@@ -0,0 +1,10 @@
+package tech.chaosmin.framework.module.cdpt.entity.channel.request
+
+/**
+ * 大地保险对接请求体 <p>
+ * <p>
+ *
+ * @author Romani min
+ * @since 2021/6/16 10:58
+ */
+class DDReq : BaseChannelReq()
---
+++ src/main/kotlin/tech/chaosmin/framework/module/cdpt/entity/channel/response/DDResp.kt
@@ -0,0 +1,10 @@
+package tech.chaosmin.framework.module.cdpt.entity.channel.response
+
+/**
+ * 大地保险对接响应体 <p>
+ * <p>
+ *
+ * @author Romani min
+ * @since 2021/6/16 11:01
+ */
+class DDResp : BaseChannelResp()

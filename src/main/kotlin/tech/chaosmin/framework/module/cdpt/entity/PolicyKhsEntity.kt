01efb4d9ff402dc3149f6a2d15bb86e23f1ea243
Romani <chaosmin@hotmail.com>
2021-06-24 00:23:14 +0800

    [BUG]mapstruct兼容性修改

--- src/main/kotlin/tech/chaosmin/framework/module/cdpt/entity/PolicyKhsEntity.kt
+++ src/main/kotlin/tech/chaosmin/framework/module/cdpt/entity/PolicyKhsEntity.kt
@@ -1,6 +1,8 @@
 package tech.chaosmin.framework.module.cdpt.entity
 
 import tech.chaosmin.framework.base.BaseEntity
+import tech.chaosmin.framework.module.cdpt.domain.enums.PolicyKhsEnum
+import java.util.*
 
 /**
  * @author Romani min
@@ -9,6 +11,15 @@ import tech.chaosmin.framework.base.BaseEntity
 class PolicyKhsEntity(id: Long? = null) : BaseEntity(id) {
     /** 保单ID */
     var policyId: Long? = null
+
+    /** 可回溯材料类型 */
+    var khsType: PolicyKhsEnum? = null
+
+    /** 可回溯材料生成时间 */
+    var fileTime: Date? = null
+
+    /** 资源地址 */
+    var resourceUrl: String? = null
 
     /** 订单号 */
     var orderNo: String? = null

e9c331845138d72772d539222e4dde14ef136064
Romani <chaosmin@hotmail.com>
2021-10-03 16:44:13 +0800

    [逻辑修复] 允许更新已支付成功的交易记录

--- src/main/kotlin/tech/chaosmin/framework/module/payment/domain/service/impl/PaymentTransServiceImpl.kt
+++ src/main/kotlin/tech/chaosmin/framework/module/payment/domain/service/impl/PaymentTransServiceImpl.kt
@@ -4,9 +4,6 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import tech.chaosmin.framework.base.enums.ErrorCodeEnum
-import tech.chaosmin.framework.base.enums.TransactionStatusEnum
-import tech.chaosmin.framework.exception.FrameworkException
 import tech.chaosmin.framework.module.payment.domain.dao.PaymentTransactionDAO
 import tech.chaosmin.framework.module.payment.domain.dataobject.PaymentTransaction
 import tech.chaosmin.framework.module.payment.domain.service.PaymentTransService
@@ -27,10 +24,10 @@ open class PaymentTransServiceImpl : ServiceImpl<PaymentTransactionDAO, PaymentT
             logger.warn("该订单号[$outTradeNo]存在多笔交易记录, 请检查数据!")
         } else {
             val paymentTransaction = list.first()
-            if (TransactionStatusEnum.SUCCESS.getCode() == paymentTransaction.status) {
-                logger.error("该订单号[$outTradeNo]已支付成功, 无法更新状态!")
-                throw FrameworkException(ErrorCodeEnum.STATUS_ERROR.code, "支付记录状态已为成功")
-            }
+//            if (TransactionStatusEnum.SUCCESS.getCode() == paymentTransaction.status) {
+//                logger.error("该订单号[$outTradeNo]已支付成功, 无法更新状态!")
+//                throw FrameworkException(ErrorCodeEnum.STATUS_ERROR.code, "支付记录状态已为成功")
+//            }
             logger.info("订单号[$outTradeNo]的交易记录状态由(${paymentTransaction.status})变为(${transaction.status})")
             transaction.id = paymentTransaction.id
             baseMapper.updateById(transaction)
